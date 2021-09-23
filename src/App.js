51767331bfae387e1a794accc851278365dc466c
james <engrjayt200@gmail.com>
2021-09-23 17:31:30 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import landing from "./assets/Landing page image.png";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <img className="Image" src={landing} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
