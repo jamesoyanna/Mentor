c480b85465143a7b2930ed3fecdf03b29a47e40f
james <engrjayt200@gmail.com>
2021-09-23 17:36:02 +0100

    Update app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import landing from "./assets/Landing page image.png";
 import './App.css';
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <img className="Image" src={landing} />
+       <img className="Image" src={landing}  alt="new-log"/>
       </header>
     </div>
   );
