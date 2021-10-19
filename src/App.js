7d493ea328783d3913a29841df896b9e4bfc817a
james <engrjayt200@gmail.com>
2021-10-19 19:21:32 +0100

    New commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
-import landing from "./assets/Landing page image.png";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <img className="Image" src={landing}  alt="new-log"/>
+        <div className="container">
+          <h3>Coming soon ...</h3>
+          <h4>Mentor App</h4>
+          <p>Build a remarkable career.... Get mentored</p>
+         
+        </div>
       </header>
     </div>
   );
